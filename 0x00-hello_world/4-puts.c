5f16f5bc50b6d35ef7a32d4cbb9fcc150c8870d0
ubuntu <caitleighferrey@gmail.com>
2023-02-17 00:50:22 +0530

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - prints "Programming is like building a multilingual 
- * puzzle, followed by a newline.
- * Return: ALways 0.
+ * main - main block 
+ * Return: 0
  */
-
-
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
