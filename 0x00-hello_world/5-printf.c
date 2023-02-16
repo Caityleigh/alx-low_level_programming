ec95a5c520b5faec6f2bf3b8d1b2c30a3ce6156d
ubuntu <caitleighferrey@gmail.com>
2023-02-17 01:36:08 +0530

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - prints "with proper gammar, but the outcome is 
+ * main - prints "with proper grammar, but the outcome is 
  * a piece of art," followed newline 
- * Return: ALways 0.
+ * Return: Always 0.
  *
  */
 
 int main(void)
 
 {
-	printf("with proper grammar, but the outcome is a piece of art,|n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
