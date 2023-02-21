14bfc201b77d773ee1e353fab27ade80d8ac3762
Caityleigh <caitleighferrey@gmail.com>
2023-02-21 10:11:12 -0800

    Searching for upper or lowercase

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Function that adds two integers and returns the result
+ * @x - The the first number computed
+ * @y - The the second number computed
+ * Return: Always 0
+ */
+
+int add(int x, int y)
+
+{
+	return (x + y);
+
+}
