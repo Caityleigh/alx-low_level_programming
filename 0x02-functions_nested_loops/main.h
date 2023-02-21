99c39afb7291d698420c6a76abc84c22cc70f602
Caityleigh <caitleighferrey@gmail.com>
2023-02-21 05:50:34 -0800

    _putchar C-program

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,41 +1,25 @@
-#include <stdio.h>
-
-
-// Function prototypes
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
-int divide(int a, int b);
-
-// Prototype for _putchar function
 int _putchar(char c);
 
-// Main function
-int main() {
-  // Code goes here
-  return 0;
-}
+void print_alphabet(void);
 
-// Function definitions
-int add(int a, int b) {
-  return a + b;
-}
+void print_alphabet_x10(void);
 
-int subtract(int a, int b) {
-  return a - b;
-}
+int _islower(int c);
 
-int multiply(int a, int b) {
-  return a * b;
-}
+int _isalpha(int c);
 
-int divide(int a, int b) {
-  return a / b;
-}
+int print_sign(int n);
 
-// _putchar function definition
-int _putchar(char c) {
-  // Code to output a character goes here
-  return 0;
-}
+int _abs(int);
 
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
