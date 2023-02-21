00d319868cdd55cab6ee82636ec836a5a6258272
Caityleigh <caitleighferrey@gmail.com>
2023-02-21 04:40:26 -0800

    Functions and Putchar prototype

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+// Function prototypes
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
+
+// Prototype for _putchar function
+int _putchar(char c);
+
+// Main function
+int main() {
+  // Code goes here
+  return 0;
+}
+
+// Function definitions
+int add(int a, int b) {
+  return a + b;
+}
+
+int subtract(int a, int b) {
+  return a - b;
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
+
+int divide(int a, int b) {
+  return a / b;
+}
+
+// _putchar function definition
+int _putchar(char c) {
+  // Code to output a character goes here
+  return 0;
+}
+
