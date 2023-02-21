e2fd2e494dbbffed5c0be0fc8d49d9175eba4947
Caityleigh <caitleighferrey@gmail.com>
2023-02-21 07:03:00 -0800

    Alphabet_x10 C-program

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char i;
+	int c = 0;
+
+	while (c <= 9)
+	{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar('i');
+	}
+	_putchar('\n');
+	i++;
+	}
+}
