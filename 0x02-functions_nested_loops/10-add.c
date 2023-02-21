0e05024cd8cc4f3593c33fff3d02bbbfcb8d669d
Caityleigh <caitleighferrey@gmail.com>
2023-02-21 10:13:39 -0800

    C- program

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,14 +3,14 @@
 
 /**
  * add - Function that adds two integers and returns the result
- * @x - The the first number computed
- * @y - The the second number computed
+ * @n1 - The the first number computed
+ * @n2 - The the second number computed
  * Return: Always 0
  */
 
-int add(int x, int y)
+int add(int n1, int n2)
 
 {
-	return (x + y);
+	return (n1 + n2);
 
 }
