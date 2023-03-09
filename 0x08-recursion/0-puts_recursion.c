628cd1320d6b0e193b72d34f02002d4b27a29899
Caityleigh <caitleighferrey@gmail.com>
2023-03-09 11:40:55 -0800

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,17 @@
 #include "main.h"
-/**
- * _print_rev_recursion - prints the strings in reverse form
- * @s: string input
- */
-void _print_rev_recursion(char *s)
-{
-	if (s[0] != '\0')
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
-}
 
+/**
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
