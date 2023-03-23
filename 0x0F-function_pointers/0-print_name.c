504ed24d0a371d15b9e3ad249048c6ea9b749292
Caityleigh <caitleighferrey@gmail.com>
2023-03-22 17:32:09 -0700

    Code that takes Function that takes a name and special function to print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,14 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
-
 /**
- * print_name - Prints a name.
- * @name: Name to be printed.
- * @f: Pointer to a function that prints a name.
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
  */
-
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return 0;
-
+	if (!name || !f)
+		return;
 	f(name);
 }
