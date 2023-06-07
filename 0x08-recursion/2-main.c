1d8ae26bca117cf715726ad9dd700a0238ea0acc
Caityleigh <caitleighferrey@gmail.com>
2023-06-07 04:44:08 -0700

    Returning the length of a string while using recursions.

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int a;
+
+	a = _strlen_recursion("Dreams feel real while we're in them");
+	printf("%d\n", a);
+	return (0);
+}
