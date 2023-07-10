b326e8a4d586aef80dafe626605ce49d17340b5c
Caityleigh <caitleighferrey@gmail.com>
2023-07-09 19:21:46 -0700

    Task 2 - A function that returns the value of a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,17 +14,14 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask;
-	int bit_value;
-
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
 
-	mask = 1UL << index;
-	bit_value = (n & mask) ? 1 : 0;
+	if ((n & (1 << index)) == 0)
+		return (0);
 
-	return (bit_value);
+	return (1);
 }
 
