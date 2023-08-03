e0be02926bf2252322313ad73dcc9baf09a03ae2
Caityleigh <caitleighferrey@gmail.com>
2023-08-03 16:31:16 -0700

    Task 4 - A function that clears a byte

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * clear_bit - A function that sets the value of a bit
+ * to 0 at a given index.
+ *
+ * @n: A pointer to a bit.
+ * @index: The index that sets the values.
+ *
+ * Return: -1 if error occurs, otherwise 1 if Success
+ */
+
+int clear_bit(unsigned long int  *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+
+	*n &= ~(1 << index);
+
+	return (1);
+}
